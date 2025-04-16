8d880ce44cbaf71bb84207e3e1fe7c1e6532bb26
Max Schulten <maxunlimited1222@gmail.com>
2025-04-15 22:32:30 -0400

    Wrote a quick script to combine all the checkpointed csv files; did some playing around with YAKE for RAG. I think that's what I'll go with

--- scripts/csv_concat.py
+++ scripts/csv_concat.py
@@ -4,12 +4,27 @@ import re
 
 def concat_csv(dir, pattern = ".*"):
     try:
-        file_names = [f for f in os.listdir() if os.path.isfile(os.path.join(dir, f)) and bool(re.search(pattern, f))]
+        file_names = [f for f in os.listdir(dir) if os.path.isfile(os.path.join(dir, f)) and bool(re.search(pattern, f))]
     except FileNotFoundError:
         return None
     except NotADirectoryError:
         return None
     
-    print(file_names)
+    
+    dfs = []
+    for filename in file_names:
+        path = dir + f"/{filename}"
+        df = pd.read_csv(path)
+        dfs.append(df)
+        
+    final_df = pd.concat(dfs)
+
+    try:
+        final_df.to_csv("../test.csv")
+        return True
+    except:
+        print("something went wrong!")
+        return False
+        
 
 concat_csv("./data/proof_wiki_definitions", pattern = ".*.csv$")
