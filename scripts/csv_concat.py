b4716bc1a90e4579749028513c3f61d73ff6d47f
Maximilian Schulten <maximilian.schulten1@gmail.com>
2025-04-20 10:54:19 -0400

    Concatenated all definitions pulled from proof wiki, ended up wtih ~28,000 definitions

--- scripts/csv_concat.py
+++ scripts/csv_concat.py
@@ -2,7 +2,7 @@ import pandas as pd
 import os
 import re
 
-def concat_csv(dir, pattern = ".*"):
+def concat_csv(dir, outdir, pattern = ".*"):
     try:
         file_names = [f for f in os.listdir(dir) if os.path.isfile(os.path.join(dir, f)) and bool(re.search(pattern, f))]
     except FileNotFoundError:
@@ -20,11 +20,11 @@ def concat_csv(dir, pattern = ".*"):
     final_df = pd.concat(dfs)
 
     try:
-        final_df.to_csv("../test.csv")
+        final_df.to_csv(outdir)
         return True
     except:
         print("something went wrong!")
         return False
         
 
-concat_csv("./data/proof_wiki_definitions", pattern = ".*.csv$")
+concat_csv("./data/proof_wiki_definitions", "./final_data/proof_wiki_definitions.csv", pattern = ".*.csv$")
