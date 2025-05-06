a3adae347a11a6e17991c57b1ce9067a3ef18649
Maximilian Schulten <maximilian.schulten1@gmail.com>
2025-05-05 21:50:44 -0400

    Changing gitignore

--- app/app.py
+++ app/app.py
@@ -1,7 +1,31 @@
 from fastapi import FastAPI
+import keyword_extraction as kwe
+from pydantic import BaseModel
+import chromadb
 
+# Initialize fast api application
 app = FastAPI()
 
-@app.get('/')
-def test():
-    return {"Test", "test"}
+# Initialize the vector database using persistent vectordb
+client = chromadb.PersistentClient(path = "vectordb")
+
+# Query structure
+class Query(BaseModel):
+    prompt: str
+
+# Main endpoint for responding to queries
+@app.post('/query')
+async def query(prompt: Query):
+
+    # get keywords
+    keywords = kwe.extractKeywords(prompt.prompt)
+    
+    # Embed keywords
+    kw_embeds = embedder.embed(keywords)
+
+    return(
+        {
+            "prompt": prompt.prompt,
+            "keywords": keywords
+        }
+    )
