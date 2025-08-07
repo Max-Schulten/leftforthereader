9e512d47b38065999f9805f5d517003fab3a612e
Maximilian Schulten <maximilian.schulten1@gmail.com>
2025-08-07 11:33:14 -0400

    Adding environment variable for prod

--- app/ui.py
+++ app/ui.py
@@ -2,8 +2,9 @@ import streamlit as st
 import requests
 import re
 import time
+import os
 
-API_URL = "http://localhost:8000/"
+API_URL = os.getenv("API_URL", "http://localhost:8000/")
 st.set_page_config(page_title="L4TR", page_icon="📖")
 
 model_name = None
